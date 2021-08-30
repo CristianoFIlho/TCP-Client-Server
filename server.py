b7e3ddbc39e16a5343dacf338131bbf35bedc1e1
Cristiano Filho <54041918+CristianoFIlho@users.noreply.github.com>
2021-08-29 21:55:15 -0300

    server python

---
+++ server.py
@@ -0,0 +1,16 @@
+import socket
+
+if  __name___ == "__main__":
+    ip = "127.0.0.1"
+    port = 1234
+    
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((ip, port))
+    server.listen(5)
+
+
+    while True:
+        client, addr = server.accept()
+        print("Client connected: {address[0]:address[1]}")
+
+
